dbc8c25ac09b108406c989aeb724430b143cc39a
Celeekeybor <celestinbor02@gmail.com>
2023-05-24 07:51:57 -0700

    header file

--- celem.h
+++ celem.h
@@ -10,25 +10,19 @@
 #include <sys/stat.h>
 
 /*PROTOTYPES*/
-int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
-char *search_path(char *command);
-char **_split(char *str, char *sep);
-char *_strcat(char *dest, char *src);
-int _strlen(char *s);
+int compare_str(char *s1, char *s2);
+char *copy_str(char *dest, char *src);
+char *find_source(char *command);
+char **divide_str(char *str, char *sep);
+char *add_str(char *dest, char *src);
+int len_str(char *s);
 struct Person {
   char name[50];
   int citNo;
   float salary;
 } person1;
-char *_getenv(char *env_var);
-void *_calloc(unsigned int nmemb, unsigned int size);
-void _env(void);
-int execute(char **args);
-int empty_line(char *buff);
-
-/* GLOBAL VAR*/
-extern char **environ;
-
-#endif
-
+char *fetch_env(char *env_var);
+void *space_config(unsigned int nmemb, unsigned int size);
+void environment(void);
+int implement(char **args);
+int blank_space(char *buff);
