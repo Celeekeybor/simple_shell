8b8c1e98857aae07e8984e80e166d684801738c0
Celeekeybor <celestinbor02@gmail.com>
2023-05-24 01:00:27 -0700

    split input string

--- splitit.c
+++ splitit.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <string.h>
 #define BUFFER_SIZE 256
+/**
+ * Splits the input string into individual arguments based on spaces, tabs, and newlines.
+ *
+ * @param input The input string to be split into arguments.
+ * @return A pointer to an array of strings representing the individual arguments.
+ *         The last element of the array is set to NULL.
+ */
 char* split_arguments(char* input)
 {
 static char* arguments[BUFFER_SIZE];
